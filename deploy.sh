b045d1a8345961e026cada763280b9a66148bd59
unknown <samuel.pletain@gmail.com>
2022-07-29 16:45:56 -0600

    Clean-up unecessary files

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+
+git push -f git@github.com:samuelpletain/FEM-ping-single-column.git main:gh-pages
+
+cd -
